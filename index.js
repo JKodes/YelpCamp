cf9ea477bbb87e56ac3120a0670074586bdc09a3
unknown <jawanistanford@students.ptechnyc.org>
2023-02-24 13:22:49 -0500

    require models for the campground

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
-
+const Campground = require('./models/campground')
 
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "connection error:"))
@@ -18,6 +18,12 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makeCampground', async (req, res) =>{
+    const camp = new Campground({title: 'My Backyard'})
+    await camp.save()
+    res.send(camp_added)
 })
 
 app.listen(3000, () =>{
