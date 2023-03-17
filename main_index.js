92d9a48b158eb7b0d9a2996f82c5850ab2ea0c91
unknown <jawanistanford@students.ptechnyc.org>
2023-03-16 21:22:04 -0400

    make change to the error handle middleware

--- main_index.js
+++ main_index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const ejsMate = require('ejs-mate')
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 const catchAsync = require('./utils/catchAsync')
+const ExpressError = require('./utils/ExpressError')
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
 
@@ -38,16 +39,17 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if(!req.body.campground) throw new ExpressError('Invalid Campground data', 400)
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
 
 }))
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground })
-})
+}))
 
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
@@ -67,8 +69,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds')
 }))
 
+
+app.all('*', (req, res, next)=>{
+    next(new ExpressError('Page NOT Found', 404))
+})
+
 app.use((err, req, res, next) => {
-    res.send('Oh Boy, somwething is wrong')
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh no!!!'
+    res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () =>{
