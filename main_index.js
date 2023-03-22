7f3959cb5a5c6368eeec193544a47ec8eefb59da
unknown <jawanistanford@students.ptechnyc.org>
2023-03-21 22:34:26 -0400

    create a route that give reviews to the specific campground being seen

--- main_index.js
+++ main_index.js
@@ -7,6 +7,7 @@ const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
+const Review = require('./models/review')
 
 
 const db = mongoose.connection
@@ -84,7 +85,12 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
 }))
 
 app.post('/campgrounds/:id/reviews', catchAsync(async(req, res)=> {
-    
+    const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review)
+    campground.reviews.push(review)
+    await review.save()
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.all('*', (req, res, next)=>{
