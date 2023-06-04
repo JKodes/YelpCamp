64da6d4c10e80ac97251fa79a0ef241dfc719cf4
JKodes <adrainford09@gmail.com>
2023-06-03 23:49:37 -0400

    add new middleware function so user can return to page previously before prompted to signing

--- middleware.js
+++ middleware.js
@@ -2,8 +2,17 @@
 
 module.exports.isLoggedIn = (req, res, next) =>{
     if(!req.isAuthenticated()){
-        req.flash('error', 'you mus tbe signed in')
+        req.session.returnTo = req.originalUrl
+        req.flash('error', 'you must be signed in')
         return res.redirect('/login')
     }
     next()
+}
+
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
+    }
+    next();
 }
