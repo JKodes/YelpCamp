721437173cdd6b3b72d39759ac667b4b1904d6cf
JKodes <adrainford09@gmail.com>
2023-06-09 15:55:15 -0400

    had to populate author of a review

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -27,7 +27,9 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 }))
 
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author')
+    const campground = await Campground.findById(req.params.id).populate({path:'reviews', populate: {
+        path: 'author'
+    } }).populate('author')
     if(!campground){
         req.flash('error', 'Campground not available')
         return res.redirect('/campgrounds')
