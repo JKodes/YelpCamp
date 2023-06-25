e80722af39d238eaf2049c45dcce8d7ee0e7e593
JKodes <adrainford09@gmail.com>
2023-06-25 13:48:21 -0400

    required multer to campground so user can upload multi pictures of campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,13 +2,19 @@ const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
 const campgrounds = require('../controllers/campgrounds')
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 const {isLoggedIn, validateCampground, isAuthor} = require('../middleware')
 const Campground = require('../models/campground')
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    //.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files)
+        res.send("IT WORKED")
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
