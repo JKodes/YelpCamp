587b49793c1946bef5c9d169cacceb568d627e58
JKodes <adrainford09@gmail.com>
2023-08-03 12:18:50 -0400

    add more campgrounds so map can display more clusters

--- seeds/index.js
+++ seeds/index.js
@@ -15,7 +15,7 @@ const nameOfCamp = array => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async () => {
     await Campground.deleteMany({})
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 450; i++) {
         const randomcity = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10
         const newCamp = new Campground({
@@ -26,7 +26,10 @@ const seedDB = async () => {
             price,
             geometry: {
                 type: "Point",
-                coordinates: [-113.1331, 47.0202]
+                coordinates: [
+                    cities[randomcity].longitude, 
+                    cities[randomcity].latitude,
+                ]
             },
             images: [
                 {
