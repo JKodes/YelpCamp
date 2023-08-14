2d9f5e46da9801776195d14fa5f7385561cfb962
JKodes <adrainford09@gmail.com>
2023-08-14 15:27:22 -0400

    change cdn link nav menu can work

--- main_index.js
+++ main_index.js
@@ -67,17 +67,17 @@ const scriptSrcUrls = [
     "https://api.mapbox.com/",
     "https://kit.fontawesome.com/",
     "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
+    "https://cdn.jsdelivr.net/",
 ];
 //This is the array that needs added to
 const styleSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://kit-free.fontawesome.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://kit-free.fontawesome.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
